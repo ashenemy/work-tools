e12c4cc3fa8ddc0ec0d0c02aef070477049d9119
ashenemy <ashenemy@gmail.com>
2026-03-02 12:17:20 -0800

    fix: cleanup fiexes

--- packages/libs/extra-db/src/@types/index.ts
+++ packages/libs/extra-db/src/@types/index.ts
@@ -1,6 +1,6 @@
 import { Schema, Model, HydratedDocument, SchemaDefinitionType, SchemaDefinition } from 'mongoose';
 
 export type MongoDoc<T, M = {}> = HydratedDocument<T, M>;
-export type MongoModel<T, M = {}> = Model<MongoDoc<T, M>, {}, M>;
-export type MongoSchema<T, M = {}> = Schema<MongoDoc<T, M>, MongoModel<T, M>>;
+export type MongoModel<T, M = {}> = Model<T, {}, M>;
+export type MongoSchema<T, M = {}> = Schema<T, MongoModel<T, M>, M>;
 export type MongoSchemaDef<T> = SchemaDefinition<SchemaDefinitionType<T>>;
--- packages/libs/extra-db/src/lib/utils/id-transform.ts
+++ packages/libs/extra-db/src/lib/utils/id-transform.ts
@@ -1,4 +1,4 @@
-export const idTransform = (doc: unknown, ret: Record<any, any>) => {
+export const idTransform = (doc: unknown, ret: Record<any, unknown>) => {
     if (ret._id != null) {
         ret._id = ret._id.toString();
     }
