7b25b1b00856ab521d501382111e3e206ff79a03
ashenemy <ashenemy@gmail.com>
2026-03-06 19:07:17 -0800

    refactoring

--- packages/libs/extra-db/src/index.ts
+++ packages/libs/extra-db/src/index.ts
@@ -1,6 +1,4 @@
-export { getClickHouseTableToken } from './lib/utils/get-clickhouse-table-token';
 export { ExCrudService } from './lib/ex-crud.service';
-export { getMongoCollectionToken } from './lib/utils/get-mongo-collection-token';
 export { ExSchema } from './lib/ex-schema.class';
 export type { MongoCreate, MongoDoc, MongoFilter, MongoId, MongoInsertOptions, MongoModel, MongoProjection, MongoQueryOptions, MongoSaveOptions, MongoSchema, MongoSchemaDef, MongoUpdate } from './@types';
 
