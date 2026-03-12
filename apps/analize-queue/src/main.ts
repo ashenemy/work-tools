cebf9fcbc56a679be08fae94edbe6fd6b489694f
ashenemy <ashenemy@gmail.com>
2026-03-12 20:26:41 +0400

    fix: biome fix

---
+++ apps/analize-queue/src/main.ts
@@ -0,0 +1,1 @@
+console.log('Analize queue app running')
---
+++ apps/analize-queue/webpack.config.js
@@ -0,0 +1,31 @@
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const { join } = require('path');
+
+module.exports = {
+    output: {
+        path: join(__dirname, 'dist'),
+        clean: true,
+        ...(process.env.NODE_ENV !== 'production' && {
+            devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+        }),
+    },
+    plugins: [
+        new NxAppWebpackPlugin({
+            target: 'node',
+            compiler: 'tsc',
+            main: './src/main.ts',
+            tsConfig: './tsconfig.app.json',
+            assets: [
+                {
+                    input: 'apps/config-generator/build',
+                    glob: 'config.toml',
+                    output: '.',
+                },
+            ],
+            optimization: false,
+            outputHashing: 'none',
+            generatePackageJson: false,
+            sourceMap: true,
+        }),
+    ],
+};
